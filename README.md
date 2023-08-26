578e0c43c92b015353444306faab74c825d21f0b
0xdreadnaught <brianrpeters@gmail.com>
2023-08-26 00:09:52 -0400

    removed local SSH auth pinning. removed list-users for now.

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ This repository contains the Dockerized services for the Artifact Toolkit projec
 
 ## Services
 
-- `artifacttoolkit-auth`: Authentication service.
+- `artifacttoolkit-auth`: Krypte-Server pubkey/JSON based authentication service.
 
 ## Setup
 
 1. Clone the repository.
 2. Copy `.env-sample` to `.env` and update the environment variables as needed.
 3. Run `docker-compose up --build` to start the services.
+4. Add users to user_data.json
 
 ## Development
 
@@ -19,8 +20,13 @@ Currently focusing on the `artifacttoolkit-auth` service. Other services are pla
 ## Directory Structure
 
 ./
+
 ├── README.md
+
 ├── .env-sample
+
 ├── docker-compose.yml
+
 └── artifacttoolkit-auth/
+
     └── Dockerfile
--- artifacttoolkit-auth/krypt-server.py
+++ artifacttoolkit-auth/krypt-server.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3
-
+# Updated SSH server script to reflect the new user authentication and authorization logic
+import json
 import paramiko
 import socket
 import threading
-import json
 from os.path import exists
-from paramiko import RSAKey
-from base64 import decodebytes
-import time
 from datetime import datetime
+import time
 
 def log_message(status, message):
     status_colors = {
@@ -21,7 +18,6 @@ def log_message(status, message):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"[{timestamp}] [{status_colors[status]}{status}\033[0m] {message}")
 
-# Function to read user data from JSON file
 def read_user_data():
     try:
         with open("user_data.json", "r") as f:
@@ -29,18 +25,9 @@ def read_user_data():
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
-# Function to write user data to JSON file
 def write_user_data(data):
     with open("user_data.json", "w") as f:
         json.dump(data, f)
-
-def generate_temp_key():
-    temp_key_path = "temp_server_key"
-    if not exists(temp_key_path):
-        log_message("WARN", "Server key not found. Generating a new key...")
-        temp_key = paramiko.RSAKey.generate(bits=2048)
-        temp_key.write_private_key_file(temp_key_path)
-    return temp_key_path
 
 class Server(paramiko.ServerInterface):
     def __init__(self):
@@ -48,76 +35,78 @@ class Server(paramiko.ServerInterface):
         self.username = None
 
     def check_auth_publickey(self, username, key):
+        log_message("INFO", f"check_auth_publickey called for {username}")
         self.username = username
-        log_message("INFO", f"{username} connected from {client_address[0]}") # Log connection
-
         user_data = read_user_data()
-
+        key_base64 = key.get_base64()
+        
         if username not in user_data:
             user_data[username] = {
-                "pubkeys": [],
+                "pubkeys": [""],
                 "first_seen": str(datetime.utcnow()),
                 "last_seen": str(datetime.utcnow())
             }
-
-        user_data[username]["last_seen"] = str(datetime.utcnow())
-
-        # Key validation logic here...
-        key_str = key.get_base64()
-        if key_str in user_data[username]["pubkeys"]:
-            log_message("OK", f"{username} key validation passed") # Log key validation
             write_user_data(user_data)
+            log_message("WARN", f"User {username} not found. Template created. Account pending.")
+            return paramiko.AUTH_FAILED
+        
+        if key_base64 in user_data[username]['pubkeys']:
+            log_message("OK", f"{username} key validation passed")
             return paramiko.AUTH_SUCCESSFUL
-
-        authorized_keys_path = f'/home/{username}/.ssh/authorized_keys'
-        try:
-            with open(authorized_keys_path, 'r') as file:
-                for line in file:
-                    key_parts = line.strip().split()
-                    if len(key_parts) < 2:
-                        continue
-                    key_type, key_base64 = key_parts[:2]
-                    key_data = decodebytes(key_base64.encode('utf-8'))
-                    authorized_key = RSAKey(data=key_data)
-                    if authorized_key == key:
-                        user_data[username]["pubkeys"].append(key_str)
-                        log_message("OK", f"{username} key validation passed") # Log key validation
-                        write_user_data(user_data)
-                        return paramiko.AUTH_SUCCESSFUL
-        except Exception as e:
-            log_message("FAIL", f"Error reading authorized keys for {username}: {str(e)}")
-
-        log_message("FAIL", f"{username} key validation failed") # Log key validation
-        write_user_data(user_data)
+        
+        log_message("FAIL", f"{username} key validation failed")
         return paramiko.AUTH_FAILED
 
     def get_allowed_auths(self, username):
         return 'publickey'
-
+        
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
-def handle_client(client_socket, server_key_path):
-    global client_address # Make client_address accessible to the Server class
+    def check_channel_exec_request(self, channel, command):
+        cmd_str = command.decode('utf-8')
+        user_data = read_user_data()
+
+        if self.username not in user_data:
+            channel.send("Your account is pending. Please wait for admin approval.\n")
+            self.event.set()
+            return True
+
+        if cmd_str == 'login':
+            channel.send("Login command detected\n")
+        elif cmd_str == 'list-keys':
+            channel.send(", ".join(user_data[self.username]['pubkeys']) + "\n")
+        elif cmd_str == 'purge-keys':
+            user_data[self.username]['pubkeys'] = []
+            write_user_data(user_data)
+            channel.send("All keys purged\n")
+        else:
+            channel.send(f"{cmd_str} command not found.\n")
+        
+        self.event.set()
+        return True
+
+def handle_client(client_socket):
     client_address = client_socket.getpeername()
     log_message("INFO", f"Connection accepted from {client_address}")
 
     try:
         transport = paramiko.Transport(client_socket)
         transport.load_server_moduli()
-        transport.add_server_key(paramiko.RSAKey(filename=server_key_path))
+        transport.add_server_key(paramiko.RSAKey(filename="temp_server_key"))
         server = Server()
         transport.start_server(server=server)
-
+        
         channel = transport.accept()
         if channel is not None:
             if transport.is_authenticated():
-                channel.send("\nValidated key\n")
+                channel.send("Validated key\n")
+                server.event.wait(10)
             else:
-                channel.send("\nINVALID KEY\n")
-            time.sleep(1) # Adding a delay to allow the client to receive the message
+                channel.send("INVALID KEY or ACCOUNT PENDING\n")
+            time.sleep(1)
             channel.close()
     except Exception as e:
         log_message("FAIL", f"Exception handling client: {str(e)}")
@@ -125,7 +114,6 @@ def handle_client(client_socket, server_key_path):
         transport.close()
 
 def start_server():
-    server_key_path = generate_temp_key()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('0.0.0.0', 2222))
@@ -134,7 +122,7 @@ def start_server():
 
     while True:
         client_socket, _ = server_socket.accept()
-        threading.Thread(target=handle_client, args=(client_socket, server_key_path)).start()
+        threading.Thread(target=handle_client, args=(client_socket,)).start()
 
 if __name__ == '__main__':
     start_server()
