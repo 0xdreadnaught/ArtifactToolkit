75cbfd9d36264d9df1cbf329140b2de76197a121
administrator <testing@test.com>
2023-09-09 17:29:09 -0400

    sketchy testing

--- artifacttoolkit-ingest/entrypoint.sh
+++ artifacttoolkit-ingest/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# Swap out plceholders with env ports
+# Swap out placeholders with env ports
 /etc/nginx/swap_ports.sh
 
 # Run a self test for verbosity's sake
 nginx -t
 
+# Find the installed PHP-FPM version and start it
+PHP_FPM_VERSION=$(dpkg -l | grep php-fpm | awk '{print $2}' | cut -d':' -f1)
+service $PHP_FPM_VERSION start
+
 # Launch Nginx
 nginx -g 'daemon off;'
+
